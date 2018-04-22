009502f6ae65b5146ecc40d9d439ff20887f8774
cuzz1 <cuzz1234@163.com>
2018-04-22 14:32:01 +0800

    Initial commit

---
+++ src/java/com/imooc/o2o/entity/WechatAuth.java
@@ -0,0 +1,42 @@
+package com.imooc.o2o.entity;
+
+import java.util.Date;
+
+public class WechatAuth {
+    private Long WechatAuthId;
+    private String openId;
+    private Date createtime;
+    private PersonInfo personInfo;
+
+    public PersonInfo getPersonInfo() {
+        return personInfo;
+    }
+
+    public void setPersonInfo(PersonInfo personInfo) {
+        this.personInfo = personInfo;
+    }
+
+    public Long getWechatAuthId() {
+        return WechatAuthId;
+    }
+
+    public void setWechatAuthId(Long wechatAuthId) {
+        WechatAuthId = wechatAuthId;
+    }
+
+    public String getOpenId() {
+        return openId;
+    }
+
+    public void setOpenId(String openId) {
+        this.openId = openId;
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+}
